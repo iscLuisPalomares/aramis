eb6af9ca257556800f47169e80e144cd0a0381e4
Luis Palomares <lpalomares@posey.com>
2017-08-09 18:21:00 -0700

    Requisiciones a mantenimiento 1.0

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -138,7 +138,7 @@
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(83, 17);
             this.label4.TabIndex = 8;
-            this.label4.Text = "v. beta 2.11";
+            this.label4.Text = "v. beta 2.12";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // label5
